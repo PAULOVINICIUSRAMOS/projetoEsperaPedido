c50888ff00ae4f40f62c10b1b93cd9bc56dcef22
Paulo Ramos <contato.pramos@gmail.com>
2020-07-21 21:28:48 -0300

    primeira confirmação

---
+++ Endities/Enums/OrderStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Endities.Enums
+{
+    enum OrderStatus : int
+    {
+
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Course.Endities;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Course.Endities.Enums;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+
+            Console.WriteLine(txt);
+
+
+            Console.ReadLine();
+        }
+    }
+
+}
